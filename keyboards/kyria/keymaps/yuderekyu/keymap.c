6a376e8a267aff8c2094db380c352b20239f8c78
derek yu <yuderekyu@gmail.com>
2020-10-18 17:46:41 -0500

    move shift to lower spot. move ctrl to outside key

--- keyboards/kyria/keymaps/yuderekyu/keymap.c
+++ keyboards/kyria/keymaps/yuderekyu/keymap.c
@@ -27,21 +27,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Base Layer: QWERTY
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |GUI/tab|   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  opt/esc|
+ * |GUI/tab|   Q  |   W  |   E  |   R  |   T  |                               |   Y  |   U  |   I  |   O  |   P  |  opt/esc|
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |shift/bksp|ctrl/A | S  |  D   |   F  |   G  |                            |   H  |   J  |   K  |  L |ctr/ ; : |shift/' "|
+ * |ctrl/bksp|  A | S    |  D   |   F  |   G  |                               |   H  |   J  |   K  |  L   | ; :  | shift/' "|
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | capslock |   Z  |   X  |   C  |   V  |   B  |LShift|LShift|  |LShift|LShift|   N  |   M  | ,  < | . >  | /  ? | rshift |
+ * | shift/del|   Z  |   X  |   C  |   V  |   B  |LShift|LShift|  |LShift|LShift|   N  |   M  | ,  < | . >  | /  ? | rshift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | GUI  | Del  | LGui |Space |enter|   |space |enter | bksp | esc |RShift|
+ *                        | GUI  | capslock| LGui |Space |enter|   |space |enter | bksp | esc |RShift|
  *                        |      |      | tab | Lower|Raise|   |Lower| Raise|      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
                        MT(MOD_LGUI, KC_TAB),       KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    MT(KC_ROPT, KC_ESC),
-                       MT(MOD_LSFT, KC_BSPC),  MT(MOD_LCTL, KC_A),   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    MT(MOD_RCTL, KC_SCLN), MT(MOD_RSFT, KC_QUOT),
-      KC_CAPSLOCK,                 KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LSFT,   KC_LSFT, KC_LSFT, KC_LSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-      KC_LCMD, KC_DEL, MT(MOD_LGUI, KC_TAB), LT(_LOWER, KC_SPC), LT(_RAISE, KC_ENT), LT(_LOWER, KC_SPC), LT(_RAISE, KC_), KC_BSPC,  KC_ESC, KC_RSFT
+                       MT(MOD_LCTL, KC_BSPC),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, MT(MOD_RCTL, KC_QUOT),
+                       MT(KC_LSFT, KC_DEL),                 KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LSFT,   KC_LSFT, KC_LSFT, KC_LSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+      KC_LCMD, KC_CAPSLOCK, MT(MOD_LGUI, KC_TAB), LT(_LOWER, KC_SPC), LT(_RAISE, KC_ENT), LT(_LOWER, KC_SPC), LT(_RAISE, KC_ENT), KC_BSPC,  KC_ESC, KC_RSFT
     ),
 /*
  * Lower Layer: Symbols
